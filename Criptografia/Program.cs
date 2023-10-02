8a299e6848cab961f83c49a1631aa5083608a5fd
Ramiro Quinteros <ramiroquinteros360@gmail.com>
2023-10-02 14:14:10 -0300

    first commit

---
+++ Criptografia/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Encriptar!");
+
+        Bit13("rabl");
+
+    }
+
+    static void Bit13(string cadena)
+    {
+        // Lista para guardar cada letra
+        List<string> lista = new List<string>();
+
+        string abecedario = "abcdefghijklmnñopqrstuvwxyz";
+
+        // Saca los espacios a las cadenas
+        string cadenaSinEspacios = cadena.Replace(" ", "");
+
+
+        foreach (char letra in cadenaSinEspacios)
+        {
+            // indice de cada letra que recorre el for a la cadena
+            int indice = abecedario.IndexOf(letra);
+            if (indice != -1)
+            {
+                string resultadoReal = "";
+                for (int i = 0; i < 14; i++)
+                {
+                    int nuevoIndice = (indice + i) % abecedario.Length;
+                    resultadoReal += abecedario[nuevoIndice];
+                }
+                lista.Add(resultadoReal);
+            }
+        }
+
+        foreach (var item in lista)
+        {
+            Console.Write(item);
+        }
+    }
+
+}
---
+++ Palindromo/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Palindromo");
+
+        Console.WriteLine("Ingrese un numero: ");
+        int numero = int.Parse(Console.ReadLine());
+        bool esPalindromo = IsPalindrome(numero);
+        Console.WriteLine(esPalindromo);
+    }
+
+    static bool IsPalindrome(int x)
+    {
+        // Verifica si el número es negativo.
+        if (x < 0)
+        {
+            // Los números negativos nunca son palíndromos.
+            return false;
+        }
+
+        // Inicializa dos variables, una para el número original y otra para el número invertido.
+        int original = x;
+        int inverso = 0;
+
+        // Usa un bucle while para invertir los dígitos del número original.
+        while (x != 0)
+        {
+            // Obtén el último dígito del número original.
+            int digito = x % 10;
+
+            // Agrega el dígito al número invertido, multiplicando por 10 y sumando el dígito.
+            inverso = inverso * 10 + digito;
+
+            // Elimina el último dígito del número original.
+            x /= 10;
+        }
+
+        // Paso 4: Compara el número original con el número invertido.
+        return original == inverso;
+
+    }
+}
