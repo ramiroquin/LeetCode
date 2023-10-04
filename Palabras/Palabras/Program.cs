d42e867144dbde60769ebf5d0ec9f03a2956e0ec
Ramiro Quinteros <ramiroquinteros360@gmail.com>
2023-10-04 20:00:39 -0300

    Agrego Palabras

---
+++ Palabras/Palabras/Program.cs
@@ -0,0 +1,40 @@
+﻿
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Palabra("hola fachero sos fachero");
+    }
+
+    public static void Palabra(string cadena)
+    {
+        
+        String[] palabras = cadena.Split(' ');
+        
+        Dictionary<String, int> frecuenciaPalabras = new Dictionary<String, int>();
+
+        foreach (String palabra in palabras)
+        {
+            Console.WriteLine(palabra);
+
+            if (frecuenciaPalabras.ContainsKey(palabra))
+            {
+                frecuenciaPalabras[palabra]++;
+            }
+            else
+            {
+                frecuenciaPalabras[palabra] = 1;
+            }
+        }
+
+        foreach (var kvp in frecuenciaPalabras)
+        {
+            Console.WriteLine($"la palabra {kvp.Key} se repitio {kvp.Value} veces");
+        }
+
+        
+
+    }
+}
