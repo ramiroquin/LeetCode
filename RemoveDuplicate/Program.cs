835783a08d4900d3637a98b705d56abb535a6323
Ramiro Quinteros <ramiroquinteros360@gmail.com>
2023-10-07 16:27:48 -0300

    removeDuplicate

---
+++ RemoveDuplicate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Remove duplicate");
+
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        RemoveDuplicates(nums);
+    }
+
+    static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int elementoUnico = 1;  // Al menos un elemento único (el primero)
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[elementoUnico] = nums[i];
+                elementoUnico++;
+            }
+        }
+
+        return elementoUnico;
+    }
+}
--- RomanToInt/Program.cs
+++ RomanToInt/Program.cs
@@ -15,7 +15,7 @@ internal class Program
 
         char[] cadena = s.ToUpper().ToCharArray();
 
-        Dictionary<string, int> numeroRomanos = new Dictionary<string, int>
+        IDictionary<string, int> numeroRomanos = new Dictionary<string, int>
     {
         {"I", 1},{"V", 5}, {"X", 10}, {"L", 50},
         {"C", 100}, {"D", 500}, {"M", 1000}
